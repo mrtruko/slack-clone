4e44b4dd62dac8f98a233d68f94c97fd4cae1d4a
Nicolas Gonzalez <nicolas.ignacio.gonzam@gmail.com>
2021-03-11 23:02:44 -0300

    Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route path="/room">
+            Chat Page
+          </Route>
+          <Route path="/">
+            Login Page
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
