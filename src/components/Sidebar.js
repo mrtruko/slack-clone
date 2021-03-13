94c1bb43ce2dcb5a2b94372f0a106c248c3f9539
Nicolas Gonzalez <nicolas.ignacio.gonzam@gmail.com>
2021-03-12 21:52:20 -0300

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,32 @@
+import styled from 'styled-components';
+
 function Sidebar() {
     return (
-        <div>
-            sidebar
-        </div>
+        <Container>
+            <WorkSpaceContainer>
+                <Name>
+                    Nicolas
+                </Name>
+                <NewMessage>
+
+                </NewMessage>
+            </WorkSpaceContainer>
+        </Container>
     )
 }
 
 export default Sidebar
+
+const Container = styled.div`
+    background: #3F0E40;
+`
+
+const WorkSpaceContainer = styled.div`
+    color: white;
+    height: 64px;
+    display: flex;
+    align-items: center;
+    padding-left: 19px;
+`
+const Name = styled.div``
+const NewMessage = styled.div``
